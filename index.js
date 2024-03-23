61e5f0cde75fb0263c5a8db58808a5637985cf4a
Ramil Sagidullin <rasagidullin@avito.ru>
2024-03-23 16:12:14 +0300

    feat: configured api

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ const express = require('express');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const app = express();
-const googleGenAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
+const googleGenAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const geminiPro = googleGenAI.getGenerativeModel({ model: 'gemini-pro' });
 
-app.get('/askMeetik', async (req, res, next) => {
-  const message = 'Write a story about a magic backpack.';
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  const corsWhiteList = ['https://blackraydev.github.io', 'https://localhost:5173'];
+
+  if (corsWhiteList.includes(req.headers.origin)) {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  }
+
+  next();
+});
+
+app.post('/askMeetik', async (req, res) => {
+  const { message } = req.body;
   const result = await geminiPro.generateContent(message);
   const response = await result.response;
   const text = response.text();
